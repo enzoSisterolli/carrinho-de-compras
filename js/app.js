435181dfbc8176b893f6960a86872800ec6c56c4
Enzo Sisterolli <enzors.sisterolli@gmail.com>
2026-07-10 19:14:57 -0300

    Adicionando o valor a total

--- js/app.js
+++ js/app.js
@@ -1,12 +1,13 @@
+
+let carrinho = 1400;
 function adicionar()
 {
     let total = document.getElementById("valor-total");
-    let carrinho = 1400;
-    let quantidade = document.getElementById("quantidade").value;
+    let quantidade = Math.max(1, Number(document.getElementById("quantidade").value));
     let produto = document.getElementById("produto");
-    let valorProduto = document.getElementById("produto").value;
-
+    let valorProduto = Number(document.getElementById("produto").value);
+    let carrinho = getElementById("lista-produtos");
     carrinho += valorProduto * quantidade;
-    total.innerHTML = carrinho;
+    total.innerHTML = `R$${carrinho}`;
     
 }
